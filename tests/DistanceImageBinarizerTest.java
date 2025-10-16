0b13049534b669a39cb2ee356506b886a5fe3192
Gabby-Moon <194023457+Gabby-Moon@users.noreply.github.com>
2025-10-16 11:03:10 -0700

    start of binaryArray test

--- tests/DistanceImageBinarizerTest.java
+++ tests/DistanceImageBinarizerTest.java
@@ -1,8 +1,19 @@
 // format pulled from previous assignment
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import java.awt.image.BufferedImage;
 
 class DistanceImageBinarizerTest {
+    @Test
+    void toBinaryArrayLimitThreshold() {
+        ColorDistanceFinder distanceFinder = new EuclideanColorDistance();
+        int threshold = 0;
+        int targetColor = 0x2ABBBF;
+        DistanceImageBinarizer binarizer = new DistanceImageBinarizer(distanceFinder, targetColor, threshold);
+        BufferedImage image = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
+        
+    }
+    
     @Test
     void test_name() {
     }
