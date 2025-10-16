5f90e09ce2ecb8e8d3fd84c022a1e15378ecf416
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-10-16 10:12:54 -0700

    moved tests into their own folder

---
+++ tests/DistanceImageBinarizerTest.java
@@ -0,0 +1,9 @@
+// format pulled from previous assignment
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+class DistanceImageBinarizerTest {
+    @Test
+    void test_name() {
+    }
+}
