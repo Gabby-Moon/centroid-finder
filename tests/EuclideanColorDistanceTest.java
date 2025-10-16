8d6790242de089f180730313f77370481bb517d7
Gabby-Moon <194023457+Gabby-Moon@users.noreply.github.com>
2025-10-16 12:18:12 -0700

    Added basic tests to EuclideanColorDistanceTest

--- tests/EuclideanColorDistanceTest.java
+++ tests/EuclideanColorDistanceTest.java
@@ -3,5 +3,35 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class EuclideanColorDistanceTest {
-    
+    @Test
+    void euclideanColorDistanceSameComplexColor() {
+        EuclideanColorDistance compare = new EuclideanColorDistance();
+        double real = compare.distance(0x2C33FF, 0x2C33FF);
+        double should = 0.0;
+        Assertions.assertEquals(should, real);
+    }
+
+    @Test
+    void euclideanColorDistanceSameComplexRed() {
+        EuclideanColorDistance compare = new EuclideanColorDistance();
+        double real = compare.distance(0xFF0000, 0xFF0000);
+        double should = 0.0;
+        Assertions.assertEquals(should, real);
+    }
+
+    @Test
+    void euclideanColorDistanceSameComplexGreen() {
+        EuclideanColorDistance compare = new EuclideanColorDistance();
+        double real = compare.distance(0x00FF00, 0x00FF00);
+        double should = 0.0;
+        Assertions.assertEquals(should, real);
+    }
+
+    @Test
+    void euclideanColorDistanceSameComplexBlue() {
+        EuclideanColorDistance compare = new EuclideanColorDistance();
+        double real = compare.distance(0x0000FF, 0x0000FF);
+        double should = 0.0;
+        Assertions.assertEquals(should, real);
+    }
 }
