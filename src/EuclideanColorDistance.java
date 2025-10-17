54d526dd6ddf348d489a381f502f1274fcabd8f9
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-10-17 15:09:10 -0700

    Fixed Euclidian color distance

--- src/EuclideanColorDistance.java
+++ src/EuclideanColorDistance.java
@@ -21,18 +21,22 @@ public class EuclideanColorDistance implements ColorDistanceFinder {
     public double distance(int colorA, int colorB) {
         // isolate red by masking and shifting 16 bits
         // one of the other groups mentioned an alpha channel so masking that just in case
-        int r1 = (colorA & 0xFF0000) >> 16;
-        int r2 = (colorB & 0xFF0000) >> 16; 
+        int r1 = (colorA & 0x00FF0000) >> 16;
+        int r2 = (colorB & 0x00FF0000) >> 16; 
+        // System.out.println("r1: "+ r1 + " r2: " + r2);
 
         // isolate green and shifting 8 bits
-        int g1 = (colorA & 0x00FF00) >> 8;
-        int g2 = (colorB & 0x00FF00) >> 8;
+        int g1 = (colorA & 0x0000FF00) >> 8;
+        int g2 = (colorB & 0x0000FF00) >> 8;
+        // System.out.println("g1: "+ g1 + " g2: " + g2);
 
         // isolate blue by masking, no shift required
-        int b1 = (colorA & 0x0000FF);
-        int b2 = (colorB & 0x0000FF);
+        int b1 = (colorA & 0x000000FF);
+        int b2 = (colorB & 0x000000FF);
+        // System.out.println("b1: "+ b1 + " b2: " + b2);
 
-        double distance = Math.sqrt(((r1 - r2)^2 + (g1 - g2)^2 + (b1 - b2)^2));
+        double distance = Math.sqrt((Math.pow((r1 - r2),2) + Math.pow((g1 - g2),2) + Math.pow((b1 - b2),2)));
+        // System.out.println(distance);
 
         return distance;
     }
