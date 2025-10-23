158bd9d2e13a5bebfe2e7e225b515299a72e00c3
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-10-23 11:19:40 -0700

    added experiment file for testing JavaCV

---
+++ src/main/java/io/github/GabbyMoon/centroidfinder/VideoExperiment.java
@@ -0,0 +1,11 @@
+package io.github.GabbyMoon.centroidfinder;
+
+public class VideoExperiment {
+    public static void main(String[] args) {
+
+    }
+
+    public void JavaCV() {
+        
+    }
+}
